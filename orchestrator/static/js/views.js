b44c5b4c5a520b8cab76ab37d5278a9374d92bd5
Thomas Duval <dt_github@domtombox.net>
2016-04-01 07:49:02 +0200

    Change HTML views and point to new API URLs.

--- orchestrator/static/js/views.js
+++ orchestrator/static/js/views.js
@@ -1,5 +1,3 @@
-//var ol = require('openlayers');
-
 
 var raster = new ol.layer.Tile({
     title: "Immo Spider",
@@ -9,11 +7,11 @@ var raster = new ol.layer.Tile({
 var vector = new ol.layer.Vector({
     title: 'Houses',
     source: new ol.source.Vector({
-        url: 'http://127.0.0.1:8080/api/data',
+        url: 'http://127.0.0.1:8080/api/features',
         format: new ol.format.GeoJSON()
     },
     {
-        tileOptions: {crossOriginKeyword: 'anonymous'},
+        //tileOptions: {crossOriginKeyword: 'anonymous'},
         transitionEffect: null
     }),
     style: new ol.style.Style({
@@ -32,18 +30,7 @@ var vector = new ol.layer.Vector({
 var map = new ol.Map({
     interactions: ol.interaction.defaults().extend([
         new ol.interaction.Select({
-            style: createPopupForAd
-            //style: new ol.style.Style({
-            //    image: new ol.style.Circle({
-            //        radius: 5,
-            //        fill: new ol.style.Fill({
-            //            color: '#FF0000'
-            //        }),
-            //        stroke: new ol.style.Stroke({
-            //            color: '#000000'
-            //        })
-            //    })
-            //})
+            style: selectAD
         })
     ]),
     target: 'map',
@@ -62,21 +49,21 @@ var map = new ol.Map({
     ])
 });
 
-//var popup = new ol.Overlay.Popup();
-//map.addOverlay(popup);
-
-
-function createPopupForAd(feature) {
-    //var ad = feature.get('ad');
-    console.log(vector);
+function selectAD(feature) {
+    console.log(feature);
+    console.log(feature['i']);
     var infoElement = document.getElementById('info');
-    infoElement.innerHTML = 'test';
-    //return new ol.Overlay.Popup(ad.get('address'));
+    $.getJSON('http://127.0.0.1:8080/api/data?id='+feature['i'],
+        function(data) {
+            infoElement.innerHTML = '<h1>'+data["address"]+'</h1><p>'+data["description"]+'</p>';
+            console.log(data["address"]);
+        });
+
     return new ol.style.Style({
         image: new ol.style.Circle({
             radius: 5,
             fill: new ol.style.Fill({
-                color: '#FF0000'
+                color: '#00FF00'
             }),
             stroke: new ol.style.Stroke({
                 color: '#000000'
@@ -84,15 +71,3 @@ function createPopupForAd(feature) {
         })
     })
 }
-
-
-//// select interaction working on "pointermove"
-//var selectPointerMove = new ol.interaction.Select({
-//    condition: ol.events.condition.pointerMove
-//});
-//
-//
-//map.on('singleclick', function(evt) {
-//    var prettyCoord = ol.coordinate.toStringHDMS(ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
-//    popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
-//});
