14dbca07d389309df564940a054483a62020bdde
Thomas Duval <dt_github@domtombox.net>
2016-04-03 16:58:15 +0200

    Add a thread killer.

--- orchestrator/manager.py
+++ orchestrator/manager.py
@@ -1,6 +1,7 @@
 import cherrypy
 import logging
 import os
+import sys
 from api.main import API
 from views.main import Views
 from auth import AuthController
@@ -25,15 +26,29 @@ class WatchDog(threading.Thread):
 
     def __init__(self, *args, **kwargs):
         threading.Thread.__init__(self)
+        self.__stop = False
+        self.__timer = 60
+
+    def stop(self):
+        logger.info("Stop thread...")
+        self.__stop = True
+        sys.exit(0)
 
     def run(self):
-        # TODO: add a thread killer
+        cpt = 1
         while True:
-            time.sleep(60)
+            time.sleep(1)
+            if self.__stop:
+                return
             try:
-                api.sync()
+                if cpt % self.__timer == 0:
+                    api.sync()
+                    cpt = 0
             except ConnectionError:
                 logger.warning("Unable to sync (connection error)...")
+            finally:
+                cpt += 1
+
 
 
 watchdog = WatchDog()
@@ -44,6 +59,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 cities = [
     "Vannes",
     "Lorient",
+    "Dinan",
+    "Dinard",
+    "Lamballe"
 ]
 
 logger.debug("Using configuration file {}".format(os.path.join(os.getcwd(), "main.conf")))
@@ -52,7 +70,14 @@ cherrypy.config.update(os.path.join(os.getcwd(), "main.conf"))
 cherrypy.tree.mount(views, "/", config=os.path.join(os.getcwd(), "main.conf"))
 cherrypy.tree.mount(api, "/api", config=os.path.join(os.getcwd(), "main.conf"))
 cherrypy.tree.mount(auth, "/auth", config=os.path.join(os.getcwd(), "main.conf"))
+# cherrypy.engine.signal_handler.subscribe()
+cherrypy.engine.signal_handler.set_handler('SIGINT', watchdog.stop)
+logger.info("handlers = {}".format(cherrypy.engine.signal_handler.handlers))
 
-cherrypy.engine.start()
-cherrypy.engine.block()
-watchdog.join()
+try:
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+except KeyboardInterrupt:
+    logger.info("Waiting for thread to stop...")
+    watchdog.stop()
+    # watchdog.join()
