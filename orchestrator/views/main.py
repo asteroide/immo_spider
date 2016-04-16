535430e137e9a357e5022e5f08697cfaf58fb534
asteroide <thomas.duval@orange.com>
2016-04-16 20:52:30 +0200

    Add authentication to the main page.

--- orchestrator/views/main.py
+++ orchestrator/views/main.py
@@ -1,13 +1,17 @@
 import cherrypy
 import logging
 from jinja2 import Environment, FileSystemLoader
+from auth import require, AuthController
+
 env = Environment(loader=FileSystemLoader('templates'))
 
 
 class Views(object):
 
+    auth = AuthController()
+
     @cherrypy.expose
+    @require()
     def index(self):
         tmpl = env.get_template('main.html')
         return tmpl.render()
-        # return tmpl.render(salutation='Hello', target='World')
