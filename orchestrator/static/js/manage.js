e28b02d4eae90935926a5b845c7613e7da46aac1
asteroide <dt_github@domtombox.net>
2016-04-21 22:39:16 +0200

    Add sync and purge button.

---
+++ orchestrator/static/js/manage.js
@@ -0,0 +1,16 @@
+
+function update_action_info(action) {
+    var widget = document.getElementById('action-info');
+    widget.style.display = "block";
+    widget.innerHTML = "Making the action '"+action+"', please wait...";
+    $.getJSON('http://127.0.0.1:8080/api/'+action,
+        function(data_list) {
+            widget.innerHTML = data_list.action + ": " + data_list.number + "";
+            widget.innerHTML += "<div class='right'><a href='#' onclick='close_action_info()' class='button'>Close</a></div>"
+        });
+}
+
+function close_action_info(action) {
+    var widget = document.getElementById('action-info');
+    widget.style.display = "none";
+}
