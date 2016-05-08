1fa8106fb77e9bae5e1389e92bc95b37bd1e99ed
asteroide <dt_github@domtombox.net>
2016-05-08 11:23:15 +0200

    Add the message dialog in the manage screen.

--- orchestrator/static/js/manage.js
+++ orchestrator/static/js/manage.js
@@ -7,6 +7,10 @@ function update_action_info(action) {
         function(data_list) {
             widget.innerHTML = data_list.action + ": " + data_list.number + "";
             widget.innerHTML += "<div class='right'><a href='#' onclick='close_action_info()' class='button'>Close</a></div>"
+            if (message in data_list) {
+                $( "#message" ).dialog({ width: 700, height: 500 });
+                $( "#message").innerHTML = data_list.message;
+            }
         });
 }
 
