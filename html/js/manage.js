9e590c6de64ca284441df9508258384b205bc714
asteroide <thomas.duval@orange.com>
2016-06-19 22:36:12 +0200

    Update configuration files and scripts to put apiviewer behind Apache.

---
+++ html/js/manage.js
@@ -0,0 +1,36 @@
+var ActionPanelShow = false;
+
+function update_action_info(action) {
+    var widget = document.getElementById('action-info');
+    widget.style.display = "block";
+    widget.innerHTML = "Making the action '"+action+"', please wait...";
+    $.getJSON('/api/'+action,
+        function(data_list) {
+            widget.innerHTML = data_list.action + ": " + data_list.number + "";
+            widget.innerHTML += "<div class='right'><a href='#' onclick='close_action_info()' class='button'>Close</a></div>"
+            if (message in data_list) {
+                $( "#message" ).dialog({ width: 700, height: 500 });
+                $( "#message").innerHTML = data_list.message;
+                console.log("update_action_info "+action+" "+data_list.message);
+                $( "#message").show();
+            }
+        });
+}
+
+function close_action_info(action) {
+    var widget = document.getElementById('action-info');
+    widget.style.display = "none";
+}
+
+$(document).ready(function(){
+    $("#action-button").click(function(){
+        if (ActionPanelShow) {
+            $("#action-panel").slideUp("slow");
+            ActionPanelShow = false;
+        }
+        else {
+            $("#action-panel").slideDown("slow");
+            ActionPanelShow = true;
+        }
+    });
+});
