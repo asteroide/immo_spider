953e641a8403df66be04449341607bff72f7e054
asteroide <dt_github@domtombox.net>
2016-06-24 23:26:06 +0200

    Add more tests

--- src/cobwebs/cobwebs/tests/test_config.py
+++ src/cobwebs/cobwebs/tests/test_config.py
@@ -1,6 +1,16 @@
-from cobwebs.config import get_config
+from cobwebs.config import get_config, import_plugin
 
 
 def test_get_config():
     global_config = get_config(confname="conf.yaml", confdir="conf")
     assert isinstance(global_config, dict)
+    assert "main" in global_config
+    assert "templates" in global_config['main']
+    assert "plugins" in global_config['main']
+    assert "mq_driver" in global_config['main']
+    assert "logging" in global_config
+
+
+def test_import_plugin():
+    plugins = import_plugin()
+    assert isinstance(plugins, tuple)
