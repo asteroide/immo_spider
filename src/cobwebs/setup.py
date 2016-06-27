57d121f3649bf1dd2d0e687afdd6000c8d3e1471
asteroide <dt_github@domtombox.net>
2016-06-27 23:45:51 +0200

    Delete duplicate code.

--- src/cobwebs/setup.py
+++ src/cobwebs/setup.py
@@ -18,14 +18,6 @@ if len(sys.argv) > 0:
         subprocess.call(["ls", "-l", "/etc/spider"])
 
 
-if len(sys.argv) > 0:
-    if sys.argv[1] in ("install", "develop"):
-        try:
-            os.mkdir("/etc/spider/")
-        except FileExistsError:
-            print("Warning: /etc/spider directory already exists...")
-        shutil.copy("conf/cobwebs.yaml", "/etc/spider/cobwebs.yaml")
-
 setup(
 
     name='cobwebs',
