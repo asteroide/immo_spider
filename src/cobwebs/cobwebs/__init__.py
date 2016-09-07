67306aeaa0d24e3bd8271a6154afd369d88fdb96
asteroide <dt_github@domtombox.net>
2016-09-07 18:36:33 +0200

    Initialize logging

--- src/cobwebs/cobwebs/__init__.py
+++ src/cobwebs/cobwebs/__init__.py
@@ -1,12 +1,23 @@
 import os
+import logging
 import logging.config
 from cobwebs.config import get_config
 
 __version__ = "0.1"
 
-# init logging
 
-CONF = os.getenv("SPIDER_CONF", "/etc/spider/main.yaml")
+def getLogger(name="spider"):
+    return logging.getLogger(name)
 
-global_config = get_config(CONF)
-logging.config.dictConfig(global_config['logging'])
+
+def init_logging():
+    # init logging
+    CONF = os.getenv("SPIDER_CONF", "/etc/spider/main.yaml")
+
+    global_config = get_config(CONF)
+    logging.config.dictConfig(global_config['logging'])
+
+
+init_logging()
+
+
